387deb2a544ab32840e01d33e2e83c16ec2f27a6
Battulga Myagmarjav <Tulga@Battulgas-MacBook-Pro.local>
2016-07-26 15:06:20 -0500

    removed console log

--- app/js/supportScript.js
+++ app/js/supportScript.js
@@ -3,12 +3,11 @@ $(function() {
     $('a[href*="#"]:not([href="#"])').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
             var target = $(this.hash);
-            console.log('w');
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
             if (target.length) {
                 $('html, body').animate({
                     scrollTop: target.offset().top
-                }, 2000);
+                }, 1000);
                 return false;
             }
         }
